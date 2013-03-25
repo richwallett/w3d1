c348bc615f2a7a35b0678b46aaf908b7ec01b6bb
David Xu + Rich Wallett <david+rich@appacademy.io>
2013-03-25 14:48:11 -0700

    Updated board with piece placing method and piece calling method

--- lib/reversi.rb
+++ lib/reversi.rb
@@ -1,9 +1,19 @@
 class Board
+  attr_reader :board
   def initialize
     @board = make_board
   end
 
   def make_board
+    Array.new(8) { Array.new(8) }
+  end
+  def place_piece(piece, position)
+    @board[position[0]][position[1]] = piece
+  end
+  def piece_at(position)
+    piece = @board[position[0]][position[1]]
+    return nil if piece.nil?
+    return piece.color
   end
 end
 
--- spec/reversi_spec.rb
+++ spec/reversi_spec.rb
@@ -3,11 +3,28 @@ require 'rspec'
 
 describe Board do
   subject(:board) {Board.new}
-
-  describe "#initialize" do
-    it "initializes the board" do
-      board.should_receive(:make_board)
-      board.make_board
+  let(:piece) {double("Piece", :color => :black)}
+  describe "#make_board" do
+    it "creates an 8x8 array" do
+      board.board.each do |el|
+        el.length.should == 8
+      end
+      board.board.length.should == 8
+    end
+  end
+  describe "#place_piece" do
+    it "places a piece at given location" do
+      board.place_piece(piece, [0,0])
+      board.board[0][0].should == piece
+    end
+  end
+  describe "#piece_at" do
+    it "returns nil if no piece" do
+      board.piece_at([0,0]).should be_nil
+    end
+    it "returns color of piece if piece present" do
+      board.place_piece(piece, [0,0])
+      board.piece_at([0,0]).should == :black
     end
   end
 end
